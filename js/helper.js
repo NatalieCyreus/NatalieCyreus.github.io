472f974ac8a939caf895633adb0965fcad07cd0f
Natalie Cyreus <nataliecyreus@gmail.com>
2017-07-20 11:33:21 -0400

    initial version of resumme

--- js/helper.js
+++ js/helper.js
@@ -13,25 +13,27 @@ These are HTML strings. As part of the course, you'll be using JavaScript functi
 replace the %data% placeholder text you see in them.
 */
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<span>%data%</span><hr>';
+var HTMLheaderRole = '<span class="role-text">%data%</span><hr>';
 
-var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
-var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
-var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
-var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
-var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
-var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
-var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
+var HTMLcontactGeneric = '<li class="flex-item"><span class="contact-text">%contact%</span><span class="contact-text">%data%</span></li>';
+var HTMLmobile = '<li class="flex-item"><span class="contact-text"><span class="zocial-call"></span></span><span class="contact-text">%data%</span></li>';
+var HTMLemail = '<li class="flex-item"><span class="contact-text"><span class="zocial-email"></span><span class="contact-text">%data%</span></li>';
+var HTMLtwitter = '<li class="flex-item"><span class="contact-text"><span class="zocial-twitter"></span><span class="contact-text">%data%</span></li>';
+var HTMLgithub = '<li class="flex-item"><span class="contact-text"><span class="zocial-github"></span></span><span class="contact-text">%data%</span></li>';
+var HTMLblog = '<a href="https://thecookingswede.com"><li class="flex-item"><span class="contact-text"></span><span class="contact-text">%data%</span></li></a>';
+var HTMLlocation = '<li class="flex-item"><span class="contact-text">location</span></span><span class="contact-text">%data%</span></li>';
+var HTMLskype = '<li class="flex-item"><span class="contact-text">Skype</span></span><span class="contact-text">%data%</span></li>';
 
-var HTMLbioPic = '<img src="%data%" class="biopic">';
+var HTMLbioPic = '<img src="%data%" class="bioPic">';
 var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
 
 var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-column"></ul>';
-var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
+var HTMLskills =
+ '<li class="flex-item"><span class="skills-text">%data%</span></li>';
 
 var HTMLworkStart = '<div class="work-entry"></div>';
-var HTMLworkEmployer = '<a href="#">%data%';
-var HTMLworkTitle = ' - %data%</a>';
+var HTMLworkEmployer = '<a href="https://spinviewglobal.com/">%data%</a>';
+var HTMLworkTitle = '<a href="#" > %data%</a>';
 var HTMLworkDates = '<div class="date-text">%data%</div>';
 var HTMLworkLocation = '<div class="location-text">%data%</div>';
 var HTMLworkDescription = '<p><br>%data%</p>';
@@ -40,22 +42,29 @@ var HTMLprojectStart = '<div class="project-entry"></div>';
 var HTMLprojectTitle = '<a href="#">%data%</a>';
 var HTMLprojectDates = '<div class="date-text">%data%</div>';
 var HTMLprojectDescription = '<p><br>%data%</p>';
-var HTMLprojectImage = '<img src="%data%">';
+var HTMLprojectImage = '<img class="projectImg animation-element slide-left" src="%data%">';
+
+
+var HTMLVolunteeringStart = '<div class="volunteering-entry"></div>';
+var HTMLVolunteeringTitle = '<a href="#">%data%</a>';
+var HTMLVolunteeringDates = '<div class="date-text">%data%</div>';
+var HTMLVolunteeringDescription = '<p><br>%data%</p>';
+
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
 var HTMLschoolName = '<a href="#">%data%';
-var HTMLschoolDegree = ' -- %data%</a>';
+var HTMLschoolDegree = '<a href="#" class="school-text">%data%</a>';
 var HTMLschoolDates = '<div class="date-text">%data%</div>';
 var HTMLschoolLocation = '<div class="location-text">%data%</div>';
-var HTMLschoolMajor = '<em><br>Major: %data%</em>';
+var HTMLschoolMajor = '<em class="school"><br>Major: %data%</em>';
 
 var HTMLonlineClasses = '<h3>Online Classes</h3>';
-var HTMLonlineTitle = '<a href="#">%data%';
-var HTMLonlineSchool = ' - %data%</a>';
+var HTMLonlineTitle = '<a href="#" class="school-text">%data%';
+var HTMLonlineSchool = ' <a href="#" ">%data%</a>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
-var HTMLonlineURL = '<br><a href="#">%data%</a>';
+var HTMLonlineURL = '<br><a href="#" class="online-link">%data%</a>';
 
-var internationalizeButton = '<button>Internationalize</button>';
+var internationalizeButton = '<button class="interButton">Internationalize</button>';
 var googleMap = '<div id="map"></div>';
 
 
@@ -86,9 +95,9 @@ function logClicks(x,y) {
 }
 
 $(document).click(function(loc) {
-  // your code goes here!
-});
+  logClicks(event.pageX, event.pageY);
 
+});
 
 
 /*
@@ -238,12 +247,12 @@ function initializeMap() {
 Uncomment the code below when you're ready to implement a Google Map!
 */
 
-// Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+//Calls the initializeMap() function when the page loads
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function(e) {
   //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+  map.fitBounds(mapBounds);
+});
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,316 @@
+/***************************** BIO SECTION ******************************/
+var bio = {
+    "name": "Natalie Serruya-Cyreus",
+    "role": "Web developer",
+    "contacts": {
+        "mobile": "+1-201-658-2233",
+        "email": "nataliecyreus@gmail.com",
+        "github": "/NatalieCyreus",
+        "twitter": "xxxx.twitter.com",
+        "location": "New York",
+        "blog": "thecookingswede.com",
+        "skype" : "natalie.cyreus"
+    },
+    "welcomeMessage": "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. ",
+    "skills": ["HTML", "CSS", "JavaScript"],
+    "biopic": "images/IMG_2922.JPG",
+
+    display: function() {
+
+        var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+        $("#header").prepend(formattedRole);
+
+        var formattedName = HTMLheaderName.replace("%data%", bio.name);
+        $("#header").prepend(formattedName);
+
+        var formattedImage = HTMLbioPic.replace("%data%", bio.biopic);
+        $("#header").append(formattedImage);
+
+        $("#header").append(HTMLskillsStart);
+
+        for (k = 0; k < bio.skills.length; k++) {
+          var formattedSkill = HTMLskills.replace("%data%", bio.skills[k]);
+          $("#skills").prepend(formattedSkill);
+        }
+
+
+        var formattedWelcome = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+        $("#header").append(formattedWelcome);
+
+        var email = bio.contacts.email;
+        var formattedEmail = HTMLemail.replace("%data%", email);
+        var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+        var formattedBlog = HTMLblog.replace("%data%", bio.contacts.blog);
+        var formattedPhone = HTMLmobile.replace("%data%", bio.contacts.mobile);
+        var formattedSkype = HTMLmobile.replace("%data%", bio.contacts.skype);
+        var locationNY = bio.contacts.location;
+        var formattedLocation = HTMLlocation.replace("%data%", locationNY);
+        $("#footerContacts , #topContacts").append(formattedEmail, formattedGithub, formattedBlog, formattedPhone, formattedSkype, formattedLocation);
+
+    }
+};
+
+bio.display();
+
+
+/***************************** WORK SECTION ******************************/
+var work = {
+    jobs: [{
+            employer: "Spinview",
+            title: "Co-founder",
+            location: "Stockholm, Sweden",
+            dates: "2015-2017 ",
+            description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
+
+        }
+    ],
+
+    display: function() {
+        for (var i = 0; i < work.jobs.length; i++) {
+
+            $("#workExperience").append(HTMLworkStart);
+            var EmployerFormatted = HTMLworkEmployer.replace("%data%", work.jobs[i].employer);
+            var TitleFormatted = HTMLworkTitle.replace("%data%", work.jobs[i].title);
+            var formattedWorkDates = HTMLworkDates.replace("%data%", work.jobs[i].dates);
+            var formattedWorkLocation = HTMLworkLocation.replace("%data%", work.jobs[i].location);
+            var formattedWorkDescription = HTMLworkDescription.replace("%data%", work.jobs[i].description);
+
+            var formattedEmployerTitle = EmployerFormatted + TitleFormatted + formattedWorkDates + formattedWorkLocation + formattedWorkDescription;
+            $(".work-entry:last").append(formattedEmployerTitle);
+
+        }
+    }
+};
+
+work.display();
+
+
+/*************************** PROJECT SECTION ****************************/
+
+
+var projects = {
+    "projects": [{
+            "title": "First project",
+            "dates": "june 2017",
+            "description": "Learned javascript whiledoing it.",
+            "images": ["images/computerNotes.jpeg"]
+        },
+        {
+            "title": "Second project",
+            "dates": "june 2017",
+            "description": "Learned javascript whiledoing it.",
+            "images": ["images/desk-photo.jpeg"]
+        }
+    ],
+
+
+    // Encapsulation - holding the display function inside the "projects" object.
+    display: function() {
+        for (var y = 0; y < projects.projects.length; y++) {
+            $("#projects").append(HTMLprojectStart);
+
+            var formattedProjectTitle = HTMLprojectTitle.replace("%data%", projects.projects[y].title);
+            $(".project-entry:last").append(formattedProjectTitle);
+
+            formattedProjectDates = HTMLprojectDates.replace("%data%", projects.projects[y].dates);
+            $(".project-entry:last").append(formattedProjectDates);
+
+            formattedProjectDescription = HTMLprojectDescription.replace("%data%", projects.projects[y].description);
+            $(".project-entry:last").append(formattedProjectDescription);
+
+            for (var x = 0; x < projects.projects[y].images.length; x++) {
+                var formattedProjectImage = HTMLprojectImage.replace("%data%", projects.projects[y].images[x]);
+                $(".project-entry:last").append(formattedProjectImage);
+            }
+        }
+    }
+};
+
+projects.display();
+
+
+
+
+/*************************** EDUCATION SECTION ****************************/
+
+var education = {
+    "schools": [{
+            "name": "Södertörn University",
+            "location": "Stockholm",
+            "degree": "Media and communication",
+            "majors": ["#", "#"],
+            "dates": "2013-2015",
+        },
+
+        {
+            "name": "Fullstack Academy",
+            "location": "New York",
+            "degree": "Bootcamp Prep",
+            "majors": ["#"],
+            "dates": "April-May 2017"
+
+        },
+        {
+            "name": "Spanish Course",
+            "location": "Buenos Aires",
+            "degree": "Spanish",
+            "majors": ["#"],
+            "dates": "August-September 2016"
+
+        }
+    ],
+
+    "onlineCourses": [{
+        "title": "Nanodegree front end",
+        "school": "Udacity",
+        "dates": "May 2017 - present",
+        "url": "udacity.com"
+    } ,
+    {
+        "title": "JavaScript JumpStart Online",
+        "school": "Fullstack Academy",
+        "dates": "March - April 2017",
+        "url": "https://www.fullstackacademy.com/"
+    }],
+
+    display: function() {
+
+        $("#education").append(HTMLschoolStart);
+
+        for (var n = 0; n < education.schools.length; n++) {
+
+
+
+            var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[n].name);
+            $(".education-entry:last").append(formattedSchoolName);
+            var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", education.schools[n].degree);
+            $(".education-entry:last").append(formattedSchoolDegree);
+
+            var formattedSchoolDates = HTMLschoolDates.replace("%data%", education.schools[n].dates);
+            $(".education-entry:last").append(formattedSchoolDates);
+
+            var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[n].location);
+            $(".education-entry:last").append(formattedSchoolLocation);
+
+
+
+
+            for (var h = 0; h < education.schools[n].majors.length; h++) {
+                var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", education.schools[n].majors[h]);
+                $(".education-entry:last").append(formattedSchoolMajor);
+
+            }
+
+        }
+            $("#education").append(HTMLonlineClasses);
+        for (var t = 0; t < education.onlineCourses.length; t++) {
+            $("#education").append(HTMLschoolStart);
+
+            var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[t].school);
+            $(".education-entry:last").append(formattedOnlineSchool);
+
+            var formattedOnlineSchoolTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[t].title);
+            $(".education-entry:last").append(formattedOnlineSchoolTitle);
+
+            var formattedOnlineSchoolDates = HTMLonlineDates.replace("%data%", education.onlineCourses[t].dates);
+            $(".education-entry:last").append(formattedOnlineSchoolDates);
+
+            var formattedOnlineSchoolUrl = HTMLonlineURL.replace("%data%", education.onlineCourses[t].url);
+            $(".education-entry:last").append(formattedOnlineSchoolUrl);
+
+        }
+
+    }
+
+};
+
+education.display();
+
+
+/*************************** VOLUNTEERING SECTION ****************************/
+var volunteering = {
+    "project": [{
+            "title": "Leader Red Cross",
+            "dates": "2014-2015",
+            "role": "Red Cross",
+            "description": "A language school for new arrived to Sweden. I re-opened and developed a meeting area in Flemingsberg with the goal to welcome and educate new arrived to learn about Swedish society and language."
+        },
+        {
+            "title": "Board member Red Cross Flemingsberg.",
+            "dates": "2014-2015",
+            "role": "Red Cross",
+            "description": "I was in the board of the Flemingsberg division, as one of the most active members of the board I contributed with much knowledge of the needs from the language meetings."
+        }
+    ],
+
+    // Encapsulation - holding the display function inside the "projects" object.
+    display: function() {
+        for (var w = 0; w < volunteering.project.length; w++) {
+            $("#volunteering").append(HTMLVolunteeringStart);
+
+            var formattedVolunteeringTitle = HTMLVolunteeringTitle.replace("%data%", volunteering.project[w].title);
+            $(".volunteering-entry:last").append(formattedVolunteeringTitle);
+
+            var formattedVolunteeringDates = HTMLprojectDates.replace("%data%", volunteering.project[w].dates);
+            $(".volunteering-entry:last").append(formattedVolunteeringDates);
+
+            var formattedVolunteeringDescription = HTMLprojectDescription.replace("%data%", volunteering.project[w].description);
+            $(".volunteering-entry:last").append(formattedVolunteeringDescription);
+        }
+    }
+};
+
+volunteering.display();
+
+
+
+/*************************** MAP SECTION ****************************/
+
+$("#mapDiv").append(googleMap);
+
+/******************** INTERNATIONALIZE SECTION **************************/
+
 /*
-This is empty on purpose! Your code to build the resume will go here.
- */
+$("#main").append(internationalizeButton);
+
+function inName() {
+  var splitName = bio.name.split(" ");
+   splitName[0]= splitName[0].toLowerCase();
+   var internationalizedName = splitName.join(" ");
+   return internationalizedName;
+}
+
+inName();
+
+*/
+
+/******************** ANIMATION SECTION **************************/
+
+//code from https://www.sitepoint.com/scroll-based-animations-jquery-css3/ , https://codepen.io/SitePoint/pen/yNzYqm
+
+var $animation_elements = $('.animation-element');
+var $window = $(window);
+
+function check_if_in_view() {
+  var window_height = $window.height();
+  var window_top_position = $window.scrollTop();
+  var window_bottom_position = (window_top_position + window_height);
+
+  $.each($animation_elements, function() {
+    var $element = $(this);
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = (element_top_position + element_height);
+
+    //check to see if this current container is within viewport
+    if ((element_bottom_position >= window_top_position) &&
+        (element_top_position <= window_bottom_position)) {
+      $element.addClass('in-view');
+    } else {
+      $element.removeClass('in-view');
+    }
+  });
+}
+
+$window.on('scroll resize', check_if_in_view);
+$window.trigger('scroll');
